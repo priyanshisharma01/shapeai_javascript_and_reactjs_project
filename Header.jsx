94cb354f61836c49c91676deec28f989bd9253a4
PRIYANSHI SHARMA <78258971+priyanshisharma01@users.noreply.github.com>
2021-06-08 01:05:24 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+  return (
+    <header>
+      <h1>STAR FISHERIES ORDERS</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note(){
+  return (
+    <div className = "note">
+      <h1>ORDER NUMBER 123</h1>
+      <p>
+       This is the order by Raju and Shyam and they are not going to pay for the order instead they are offering
+       Hera Feri option.
+      </p>
+    </div>
+
+    
+  );
+}
+
+export default Note;
